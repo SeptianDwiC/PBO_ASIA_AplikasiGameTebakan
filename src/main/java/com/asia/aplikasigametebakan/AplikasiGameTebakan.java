409e40af674680b2ec7e8eedc305b6a67a848003
yogaalif01 <yogaalif011110@gmail.com>
2024-07-15 09:49:09 +0700

    Upload Project

---
+++ src/main/java/com/asia/aplikasigametebakan/AplikasiGameTebakan.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.asia.aplikasigametebakan;
+
+/**
+ *
+ * @author STATION20B
+ */
+public class AplikasiGameTebakan {
+
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.start();
+    }
+}
